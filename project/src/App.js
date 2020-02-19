47f6efa786ee2966c02c8fde263d3336cea0da34
seanaleid <seanaleid@gmail.com>
2020-02-19 13:01:54 -0500

    Initialize repo
      * Adds starter code, files, and directories

---
+++ project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <h2>Start here...</h2>
+    </div>
+  );
+}
+
+export default App;
